e850914a770f7dd53bf4c2d590786bb6fc72ee2d
nakuldx10 <nakuldahiya541@gmail.com>
2026-06-21 21:24:35 +0530

    commit

--- HireHub/frontend/src/api/axios.js
+++ HireHub/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://codsoft-3moq.onrender.com'
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
